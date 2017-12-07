2bc571797e5805390786bc53babf591559876e35
Diego Nunes <diegocar.nunes@gmail.com>
2017-12-07 07:51:38 -0200

    Atividade 1245 - Accepted

--- 1245.py
+++ 1245.py
@@ -4,14 +4,12 @@ try:
         cont = 0
         a = int(input())
         for i in range(a):
-            j = tuple(input().split())
-            teste = True
-            for k in boots:
-                if (k[0] == j[0]) and (k[1] != j[1]):
-                    boots.remove(k)
-                    cont += 1
-                    teste = False
-            if teste:
+            j = input().split()
+            k = [j[0],"E" if j[1]=="D" else "D"]
+            if k in boots:
+                boots.remove(k)
+                cont += 1
+            else:
                 boots.append(j)
         print(cont)
 
