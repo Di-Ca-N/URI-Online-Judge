f620ea1a82d0c6bdc6363eef2cd38218b13c9e73
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2021-11-03 23:54:48 -0300

    1088 using python

--- Python3/1088.py
+++ Python3/1088.py
@@ -1,44 +1,25 @@
-def merge_sort_and_count(array, left, right):
-    if left >= right:
-        return 0
+def count_invs(n, array):
+    visited = set()
+    i = 0
+    total = 0
+    while i != n:
+        if i not in visited:
+            cycle = 0
+            next_i = array[i]
+            while next_i not in visited:
+                visited.add(next_i)
+                next_i = array[next_i]
+                cycle += 1
+            total += (cycle - 1) % 2
+        i += 1
+    return total
 
-    m = (left + right) // 2
-
-    inv1 = merge_sort_and_count(array, left, m)
-    inv2 = merge_sort_and_count(array, m + 1, right)
-
-    inversoes = inv1 + inv2
-
-    s1 = left
-    s2 = m + 1
-
-    while s1 <= m and s2 <= right:
-        if array[s1] <= array[s2]:
-            s1 += 1
-            continue
-        
-        inversoes += s2 - s1
-        v = array[s2]
-        for i in range(s2, s1 - 1, -1):
-            array[i] = array[i - 1]
-
-        array[s1] = v
-
-        s2 += 1
-        s1 += 1
-        m += 1
-
-    return inversoes
-   
-
-qtd, *seq = [int(x) for x in input().split()]
-
-while qtd != 0:
-    inversions = merge_sort_and_count(seq, 0, qtd - 1)
-    print(inversions)
-    if inversions % 2 == 1:
+n, *nums = [int(x) - 1 for x in input().split()]
+while n + 1 != 0:
+    invs = count_invs(n + 1, nums)
+    if invs % 2 == 1:
         print("Marcelo")
     else:
         print("Carlos")
-
-    qtd, *seq = [int(x) for x in input().split()]
+    
+    n, *nums = [int(x) - 1 for x in input().split()]
